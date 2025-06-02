c682ace566372a3062259a09a9332254197f0066
Simone Cervini <sc.cervini@gmail.com>
2025-06-02 16:42:36 +0200

    Update comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,8 +45,9 @@ function PostList() {
 function PostListItem(props: { post: Post }) {
   const { post } = props;
   // TODO: 1. Mostra per ogni post l'id, il titolo e la data di creazione con un minimo di stile
-  // TODO: 2. Rendi "focusable" il post e aggiungi un bordo per evidenziare il post focused
-  // TODO: 3. Permetti l'eliminazione dei post focused navigando con tastiera e premendo il tasto "Delete" ("Canc")
+  // TODO: 2. Rendi "focusable" il post
+  // TODO: 3. Aggiungi un bordo per evidenziare il post focused
+  // TODO: 4. Permetti l'eliminazione dei post focused navigando con tastiera e premendo il tasto "Delete" ("Canc")
   return <li>{post.title}</li>;
 }
 
